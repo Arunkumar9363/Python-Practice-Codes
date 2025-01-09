0ce2422fc313745f6c9672d4d4227b36e4487ec0
Arunkumar9363 <arunk226835@gail.com>
2025-01-08 16:43:28 -0800

    First messahge

---
+++ Day1/Basic.py
@@ -0,0 +1,14 @@
+#scripting way for writing the code:
+print('Hello World')
+
+#Procedural Way of Writing the code:
+def message():
+    print('Inside method message:Hello World')
+message()
+
+#Object Oriented way of Writing the code:
+class Demo:
+    def instance_method(self):
+        print('Object oriented way of printing: Hello World')
+d1 = Demo()
+d1.instance_method()
